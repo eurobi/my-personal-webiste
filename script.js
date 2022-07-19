c3018088228c9bf119a97cd4473618b377263c54
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-07-19 13:01:14 -0400

    working javascript to change site sections

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const selection1 = document.getElementById('selection-1');
+const selection2 = document.getElementById('selection-2');
+const selection3 = document.getElementById('selection-3');
 
-document.querySelector("body").appendChild(h2);
+const description1 = document.getElementById('description-1');
+const description2 = document.getElementById('description-2');
+const description3 = document.getElementById('description-3');
+
+selection1.addEventListener('click', function(){
+    description1.style.display = "block"
+    description2.style.display = "none"
+    description3.style.display = "none"
+ });
+
+selection2.addEventListener('click', function(){
+    description1.style.display = "none"
+    description2.style.display = "block"
+    description3.style.display = "none"
+ });
+
+ selection3.addEventListener('click', function(){
+    description1.style.display = "none"
+    description2.style.display = "none"
+    description3.style.display = "block"
+ });
+
+
